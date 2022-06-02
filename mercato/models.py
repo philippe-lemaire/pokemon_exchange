fcc737cdeed58036e13b9c5fa806a29e4c3884e4
Philippe Lemaire <philippe.lemaire@protonmail.com>
2022-06-02 16:13:48 +0200

    offer and request models ok

--- mercato/models.py
+++ mercato/models.py
@@ -1,3 +1,39 @@
+from secrets import choice
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from tinymce.models import HTMLField
+
+from .pokemon_names import pokemon_choices
+from .pokemon_natures import nature_choices
+from .system_choices import system_choices
 
 # Create your models here.
+
+
+class Offer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pokemon_name = models.CharField(max_length=4, choices=pokemon_choices, default=1)
+    pokemon_level = models.IntegerField(
+        default=1, validators=[MaxValueValidator(100), MinValueValidator(1)]
+    )
+    pokemon_shiny = models.BooleanField(default=False)
+    pokemon_ability = models.CharField(blank=True)
+    pokemon_nature = models.CharField(max_length=2, choices=nature_choices, blank=True)
+    system = models.CharField(max_length=1, choices=system_choices, default=5)
+    lat = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+    lon = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+
+    def __str__(self):
+        return f"Level {self.pokemon_level} {self.pokemon_nature} {self.pokemon_name} proposé par {self.user}."
+
+
+class Request(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pokemon_name = models.CharField(max_length=4, choices=pokemon_choices, default=1)
+    pokemon_shiny = models.BooleanField(default=False)
+    pokemon_ability = models.CharField(blank=True)
+    pokemon_nature = models.CharField(max_length=2, choices=nature_choices, blank=True)
+    lat = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+    lon = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
